4c036a9a7444559fe41d6fd84a393d9f21177054
vogat <james.voight@me.com>
2023-07-31 17:47:34 -0400

    render

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cors = require('cors');
 
+const corsOptions = {
+    origin: "https://meltingpot.onrender.com", // frontend URI (ReactJS)
+}
+
 require('dotenv').config();
 require('./config/database');
 
@@ -12,17 +16,17 @@ const apiRouter = require('./routes/api');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api', apiRouter);
 
-app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+// 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.listen(port, function () {
 	console.log(`Express app running on port ${port}`);
